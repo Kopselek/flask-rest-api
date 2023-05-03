e602e19c1d89a41b9509e2fc8ea98ad0c5584b61
KonradKacperek <konrad.kkacperek@interia.pl>
2023-05-04 01:46:16 +0200

    fix tests

--- app.py
+++ app.py
@@ -2,26 +2,24 @@ from flask import Flask, jsonify, request, redirect, url_for
 
 
 def create_app():
-    _app = Flask(__name__)
-    return _app
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return redirect(url_for('sensors_list'))
+
+    @app.route('/sensors')
+    def sensors_list():
+        return jsonify(sensors_cache), 200
+
+    @app.post('/sensor')
+    def sensor_post():
+        sensors_cache.update(request.json)
+        return '', 200
+
+    return app
 
 
-app = create_app()
+create_app()
 
 sensors_cache = {}
-
-
-@app.route('/')
-def index():
-    return redirect(url_for('sensors_list'))
-
-
-@app.route('/sensors')
-def sensors_list():
-    return jsonify(sensors_cache), 200
-
-
-@app.post('/sensor')
-def sensor_post():
-    sensors_cache.update(request.json)
-    return '', 200
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,16 +9,18 @@ data = {"0": {
 def test_index(client):
     response = client.get('/', follow_redirects=True)
     assert response.request.path == '/sensors'
+    assert len(response.history) == 1
+    assert response.status_code == 200
 
 
 def test_sensors_list(client):
     response = client.get('/sensors')
     res = json.loads(response.data)
 
-    assert type(res) is list
+    assert type(res) is dict
     assert response.status_code == 200
 
 
 def test_sensor_post(client):
-    response = client.post('/sensor', json=json.dumps(data))
+    response = client.post('/sensor', json=data)
     assert response.status_code == 200
